a3bdc11cb2fa2f4056c732971053e3f22f2718ab
Nathan Barea <nathan.barea@nhs.com.br>
2024-07-15 19:03:48 -0300

    add libreries and requirements.txt

--- analyseData.py
+++ analyseData.py
@@ -1,6 +1,19 @@
+import boto3
+import pandas as pd
+import os
+
+s3_client = boto3.client('s3')
+bucket_name = os.environ['BUCKET_NAME']
+
 def handler(event, context):
   print(event)
 
+  filename = event['filename']
+  file = s3_client.get_object(Bucket=bucket_name, Key=filename)
+  if not file: return {"status": "failure", "message": "Unable to get file from S3 Bucket"}
+
+  df = pd.read_csv(file['Body'])
   return {
-    "message": "hello from lambda 2"
+    "statusCode": "success",
+    "body": df
   }
--- getCsvFile.py
+++ getCsvFile.py
@@ -1,6 +1,26 @@
+import requests
+import json
+import boto3
+import os
+
+s3_client = boto3.client('s3')
+bucket_name = os.environ['BUCKET_NAME']
+
 def handler(event, context):
   print(event)
 
+  url = event['url']
+  filename = url.split('/')[-1]
+
+  response = requests.get(url)
+  data = response.content
+
+  s3_client.put_object(Bucket=bucket_name, Key=filename, Body=data)
+
   return {
-    "message": "hello from lambda 1"
+    "statusCode": 200,
+    "message": f"{filename} Sucessfully saved in S3",
+    "body": {
+      "filename": f"{filename}"
+    }
   }
