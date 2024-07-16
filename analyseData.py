acfa21269b1872f710dbdca57265e4b703b89a4c
Nathan Barea <nathan.barea@nhs.com.br>
2024-07-16 11:39:12 -0300

    create resource bucket S3 and continue analysis data

--- analyseData.py
+++ analyseData.py
@@ -1,6 +1,8 @@
 import boto3
 import pandas as pd
 import os
+import io
+import chardet
 
 s3_client = boto3.client('s3')
 bucket_name = os.environ['BUCKET_NAME']
@@ -8,12 +10,56 @@ bucket_name = os.environ['BUCKET_NAME']
 def handler(event, context):
   print(event)
 
-  filename = event['filename']
-  file = s3_client.get_object(Bucket=bucket_name, Key=filename)
-  if not file: return {"status": "failure", "message": "Unable to get file from S3 Bucket"}
+  # filename = event['body']['filename']
+  # file_obj = s3_client.get_object(Bucket=bucket_name, Key=filename)
 
-  df = pd.read_csv(file['Body'])
+  # csv_content = file_obj['Body'].read()
+
+  csv_content = event['body']['dataFrame']
+
+  # encoding = chardet.detect(csv_content)['encoding']
+  # data_stream = io.BytesIO(csv_content)
+    
+  df = pd.DataFrame(csv_content)
+  print(df)
+
+  vehicles = ['automovel', 'bicicleta', 'caminhao', 'moto', 'onibus']
+
+  # df = df[df['mortos'] > 0]
+  df.loc(df['mortos'] > 0)
+  df = df.sort_values(by=['data'])
+  df = df.loc[:, ['data', 'trecho', 'automovel', 'bicicleta', 'caminhao', 'moto', 'onibus', 'mortos']]
+
+  result_df = pd.DataFrame(columns=['created_at', 'road_name', 'vehicle', 'number_deaths'])
+
+  for index, row in df.iterrows():
+      created_at = row['data']
+      road_name = row['trecho']
+
+      for vehicle in vehicles:
+          number_deaths = row[vehicle]
+
+          if number_deaths > 0:
+            result_df = pd.concat([result_df, pd.DataFrame([[created_at, road_name, vehicle, number_deaths]], columns=['created_at', 'road_name', 'vehicle', 'number_deaths'])], ignore_index=True)
+
+  result_df = result_df.groupby(['created_at', 'road_name', 'vehicle']).sum().reset_index()
+
+  print(result_df)
   return {
-    "statusCode": "success",
-    "body": df
+    'statusCode': 200,
+    'message': 'Success',
+    'body': {
+      # # Inclua as informações relevantes para o usuário
+      # 'total_deaths': grouped_df['number_deaths'].sum(),  # Total de mortes
+      # 'vehicle_deaths': grouped_df.to_dict('records'),  # Detalhes por veículo, data e rua
+    }
   }
+
+
+  # return {
+  #   "statusCode": 200,
+  #   "message": f"{filename} successfully loaded from S3",
+  #   "body": {
+  #     "df": 'dataframe'
+  #   }
+  # }
--- getCsvFile.py
+++ getCsvFile.py
@@ -1,7 +1,9 @@
 import requests
-import json
 import boto3
 import os
+import chardet
+import io
+import pandas as pd
 
 s3_client = boto3.client('s3')
 bucket_name = os.environ['BUCKET_NAME']
@@ -17,10 +19,17 @@ def handler(event, context):
 
   s3_client.put_object(Bucket=bucket_name, Key=filename, Body=data)
 
+  encoding = chardet.detect(data)['encoding']
+
+  # Carregue o conteúdo do CSV em um DataFrame
+  data_stream = io.BytesIO(data)
+  df = pd.read_csv(data_stream, encoding=encoding)
+
   return {
     "statusCode": 200,
-    "message": f"{filename} Sucessfully saved in S3",
+    "message": f"{filename} sucessfully saved in S3",
     "body": {
-      "filename": f"{filename}"
+      "filename": f"{filename}",
+      "dataFrame": df.to_dict('records')
     }
   }
